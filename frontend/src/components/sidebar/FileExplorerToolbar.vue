7ebc3ad84d9b31f66bc68a571a6795f1eaf06dd7
Chad Crum <chaddcrum@gmail.com>
2025-11-28 20:13:58 -0500

    fix: correct dropdown positioning back to right side
    - Changed dropdown positioning from left: 0 back to right: 0
    - The left: 0 positioning was pushing dropdown off-screen due to full-width toolbar-menu container
    - Dropdown now appears correctly in viewport on both mobile and desktop

--- frontend/src/components/sidebar/FileExplorerToolbar.vue
+++ frontend/src/components/sidebar/FileExplorerToolbar.vue
@@ -565,7 +565,7 @@ onUnmounted(() => {
 .toolbar-dropdown {
   position: absolute;
   top: calc(100% + 0.5rem);
-  left: 0;
+  right: 0;
   display: flex;
   flex-direction: column;
   min-width: 220px;
